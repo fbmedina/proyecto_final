900e5744a77eafddc7bbc632523d3467339f6800
Francisca Medina <fbmedina@uc.cl>
2016-06-13 21:05:37 -0400

    Se hicieron las fotos por defecto de store

--- app/uploaders/cover_photo_uploader.rb
+++ app/uploaders/cover_photo_uploader.rb
@@ -24,6 +24,10 @@ class CoverPhotoUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  def default_url
+    "default_store_cover_photo.png"
+  end
+
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -24,6 +24,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  def default_url
+    "default_store_photo.png"
+  end
+
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
