05a1b959672c7b40b03e5afb73e5494bc84d752c
Francisca Medina <fbmedina@uc.cl>
2016-06-14 18:20:41 -0400

    probando callback_url 3

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,7 +240,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], 
-    callback_url: "http://184.72.95.10/users/auth/facebook/callback",
+    callback_url: "http://184.72.95.10/omniauth/facebook/callback",
     scope: 'public_profile, email, user_birthday',
     secure_image_url: true,
     image_size: {width: 400, height: 400},
